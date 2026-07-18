23a573ec1ab6edf8d7cc5cca8d6be660313bf53a
Aayushisingh936 <aayushisingh25@navgurukul.org>
2026-07-18 16:07:12 +0530

    Add files via upload

---
+++ Favorites.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+function Favorites(){
+    return(
+    <>
+    <Navbar/>
+    <div className="games-container">
+        <h1>Your Favorite Games ❤️</h1>
+        <p>No favorites added yet.</p>
+    </div>
+    </>
+    )
+}
+
+export default Favorites;
