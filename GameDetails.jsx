5cc0882c44e8e3e5e53e446edfaa9be22f56877c
Aayushisingh936 <aayushisingh25@navgurukul.org>
2026-07-18 16:06:04 +0530

    Add files via upload

---
+++ GameDetails.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+function GameDetails(){
+return(
+<>
+<Navbar/>
+<div className="games-container">
+    <h1>Game Details 🎮</h1>
+    <p>Here you can see complete information about the game </p>
+</div>
+</>
+)
+}
+
+export default GameDetails;
